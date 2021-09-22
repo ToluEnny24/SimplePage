86349215446bee7cb7a112e7faa279655db87069
FabunmiTolulopeEniola <70230223+ToluEnny24@users.noreply.github.com>
2021-09-22 03:32:13 +0100

    I have created a number of configuration files

---
+++ main.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "3.5.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file("C:/Users/USER/Downloads/polar-scene-326722-9e49595b30f6.json")
+
+  project = "polar-scene-326722"
+  region  = "us-central1"
+  zone    = "us-central1-a"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+    }
+  }
+}
+
+
